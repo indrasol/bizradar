e9c8f1ebb912778ff0665b63e2bc4ac400eeb94b
Rithin25295 <64233479+Rithin25295@users.noreply.github.com>
2025-06-07 21:17:16 -0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,12 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes.search_routes import search_router
+from routes.search_routes import search_router
 # Import our new admin routes
-from app.routes.admin_routes import router as admin_router
-from app.utils.rec_queue import start_consumer_loop
+from routes.admin_routes import router as admin_router
+from utils.rec_queue import start_consumer_loop
 
-bizapp = FastAPI()
+app = FastAPI()
 
 @app.on_event("startup")
 async def on_startup_event():
@@ -23,7 +23,7 @@ origins = [
     # "*"  # Temporarily allow all origins during development
 ]
 
-bizapp.add_middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow all origins temporarily
     allow_credentials=False,  # Must be False when using wildcard origins
@@ -32,9 +32,9 @@ bizapp.add_middleware(
 )
 
 # Include routers
-bizapp.include_router(search_router)
+app.include_router(search_router)
 # Include admin routes
-bizapp.include_router(admin_router)
+app.include_router(admin_router)
 
 if __name__ == "__main__":
     import uvicorn
